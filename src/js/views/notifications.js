a9ad3481b8dd33109df6b5b099968c65613861af
Nikki <archiektor@gmail.com>
2020-03-10 01:11:32 +0100

    - created Authentication-App

---
+++ src/js/views/notifications.js
@@ -0,0 +1,62 @@
+// let notifyContainer = document.querySelector('.notify-container');
+function getContainer() {
+    return document.querySelector('.notify-container');
+}
+
+function alertTemplate(msg, className, index) {
+    return `<div class = 'alert ${className}' data-index='${index}'>${msg}</div>`
+}
+
+function notifyContainerTemplate() {
+    return `<div class = 'notify-container' style='position: fixed; top: 10px; right: 10px; z-index: 11'></div>`;
+}
+
+function createNotifyContainer() {
+    const template = notifyContainerTemplate();
+    document.body.insertAdjacentHTML('afterbegin', template);
+}
+
+function getAlertIndex() {
+    return document.querySelectorAll('.notify-container .alert').length;
+}
+
+/**
+ *
+ * Function notify. Show notification message
+ *@param {Object} settings
+ * @param {string} settings.msg
+ * @param {string} settings.className
+ * @param {number} settings.timeout
+ *
+ * */
+
+export function notify({msg = 'Info message', className = 'alert-info', timeout = 2000} = {}) {
+    if (!getContainer()) {
+        createNotifyContainer();
+    }
+
+    const index = getAlertIndex();
+    const template = alertTemplate(msg, className);
+    const container = getContainer();
+
+    container.insertAdjacentHTML('beforeend', template, index);
+
+    setTimeout(() => deleteNotify(index), timeout)
+}
+
+export function deleteNotify(index) {
+    let alert;
+
+    if (index === undefined) {
+        alert = document.querySelector('.notify-container .alert');
+    } else {
+        alert = document.querySelector(`.notify-container .alert[data-index='${index}']`);
+    }
+
+    if (!alert) {
+        console.warn('Alert not found!');
+    }
+
+    const container = getContainer();
+    container.removeChild(alert);
+}
